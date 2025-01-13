b2f009eac1461dd109642ea7744599108a9b0164
Julio Corbaz <julio.corbaz@gmail.com>
2025-01-13 07:02:30 -0300

    OK Final

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
+// src/index.ts
 console.log("¡Hola desde TypeScript!");
+
+// Declare the type for window
+declare global {
+  interface Window {
+    info: () => void;
+  }
+}
+function usuarios() {
+  alert("¡Botón pulsado...!");
+  console.log("¡Botón pulsado!");
+}
+// Define the function and attach it to window
+window.info = usuarios;
 
 const title = document.querySelector("h1");
 if (title) {
-    title.textContent = "¡Bun es increíble!";
+  title.textContent = "¡Hola desde TypeScript!";
 }
+
+
+
